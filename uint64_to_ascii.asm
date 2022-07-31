b7cebe6c14216387bc251537393f672582bb360a
polymarv <60751513+polymarv@users.noreply.github.com>
2022-07-31 10:28:20 +0200

    Add files via upload

--- uint64_to_ascii.asm
+++ uint64_to_ascii.asm
@@ -33,12 +33,21 @@ SECTION .text
 ;-----------------------------------------------------------------------------
         global uint64_to_ascii:function
 uint64_to_ascii:
-        push    rbp
-        mov     rbp,rsp
+        mov     rax, rsi ;schreibe input in rax register
+        mov     ecx, 15 ;schreibe 15 in kleinen Teil des rcx register
+        mov     esi, 10 ; schreibe 10 in kleinen teil des rsi register
+.L2:
+        xor     edx, edx ;verhindere Fehler bei nachfolgender Division = clear rdx
+        div     rsi ;Teile rax/rsi => teile input / 10 | rax=ergebnis und rdx=rest
+        add     edx, 48 ;adde zum rest 48 (48 ascii= '0') = Konvertiere Zahl zu ascii
+        mov     BYTE [rdi+rcx], dl ;schreibe Wert von rdx in output buffer an Stelle 15
+        cmp rax, 0  ; vergleich von rax (input) und 0
+        je .finish ; wenn rax leer, dann verlasse unterprogramm
+        sub     rcx, 1 ;verringere rcx um eins um von rechts nach links das Buffer zu füllen => |    12345  |
+        jnb     .L2 ;loope bis 0 erreicht. Also von 16 -1 -1 -1 -...-> 0 erreicht
+        jmp .finish
 
-        ; YOUR CODE HERE
-
-.func_end:
-        mov     rsp,rbp
-        pop     rbp
+.finish:       
+        xor     eax, eax ;clear rax register
         ret
+
